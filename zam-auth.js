fa7af7b4cc13eb7fb0558ebdf61e39556b2d8619
Matteo Forlani <matteo@matthew5pl.net>
2025-03-09 16:15:23 +0100

    aggiunto metodo book

--- zam-auth.js
+++ zam-auth.js
@@ -171,6 +171,36 @@ class ZAMAuth {
         }
     }
 
+    async book(asset, start, end, callback) {
+        let token = this.getToken();
+
+        if(token == null) {
+            return false;
+        }
+
+        const response = await fetch(this.server + "/api/booking/book", {
+            headers: {
+                "Content-Type": "application/json"
+            },
+            method: "POST",
+            body: JSON.stringify({
+                "token": token,
+                "asset": asset,
+                "start": start,
+                "end": end
+            })
+        });
+
+        let data = await response.json();
+        if(!data.success) {
+            this.clearToken();
+        }
+
+        if(callback != undefined && callback != null) {
+            callback(data);
+        }
+    }
+
     constructor(server) {
         if(server == null || server == undefined) {
             this.server = "http://localhost:8080";
